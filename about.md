34f0d99c06256d42a10ecd1a650e13acc3343059
sfd1987 <sfd1987@gmail.com>
2014-08-14 10:30:28 -0500

    CO2 Sciences Website Upload

--- about.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-title: About
-permalink: /about/
----
-I grew up in Wilmington, Delaware, just outside of Philadelphia. I went to the same school for thirteen years from Kindergarden through high school. My class was only 58 kids and about half of them were there just as long as I was. I decided I wanted a change when it came to college and went to a bigger school all the way on the west coast, the University of Southern California. After working in Real Estate in LA for a few years, I desired something more creative. I decided it was time for a change and decided to move to Austin to pursue a career in web development.
-
---
+++ js/script.js
@@ -0,0 +1,20 @@
+var myDataRef = new Firebase('https://co2-sciences.firebaseio.com/');
+      
+
+function submitContact () {
+	console.log("ok");
+	var contact = $('#email').val();
+	myDataRef.push(contact);
+	$('#email').val("");
+      alertify.alert("Thank you for your interest");
+      alertify.log("Thank you for your interest");
+}
+
+      // $('#submit').keypress(function (e) {
+      //   if (e.keyCode == 13) {
+      //     var name = $('#nameInput').val();
+      //     var text = $('#messageInput').val();
+      //     myDataRef.set('User ' + name + ' says ' + text);
+      //     $('#messageInput').val('');
+      //   }
+      // });
