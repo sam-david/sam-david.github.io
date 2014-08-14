ba8ceb0895ddf39e8e0f4524fb8219b2a189b74a
sfd1987 <sfd1987@gmail.com>
2014-08-14 15:07:40 -0500

    Deleted unecessary jscript

--- js/script.js
+++ js/script.js
@@ -2,19 +2,8 @@ var myDataRef = new Firebase('https://co2-sciences.firebaseio.com/');
       
 
 function submitContact () {
-	console.log("ok");
 	var contact = $('#email').val();
 	myDataRef.push(contact);
 	$('#email').val("");
-      // alertify.alert("Thank you for your interest");
       alertify.log("We will keep you posted");
 }
-
-      // $('#submit').keypress(function (e) {
-      //   if (e.keyCode == 13) {
-      //     var name = $('#nameInput').val();
-      //     var text = $('#messageInput').val();
-      //     myDataRef.set('User ' + name + ' says ' + text);
-      //     $('#messageInput').val('');
-      //   }
-      // });
