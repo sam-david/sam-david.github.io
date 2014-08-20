c8a7f0c7687cd408e0f8340b5464b94c15918aa6
sfd1987 <sfd1987@gmail.com>
2014-08-20 15:14:57 -0500

    Uploaded Tesla Vehicle Comparison Site

--- js/script.js
+++
@@ -1,9 +0,0 @@
-var myDataRef = new Firebase('https://co2-sciences.firebaseio.com/');
-      
-
-function submitContact () {
-	var contact = $('#email').val();
-	myDataRef.push(contact);
-	$('#email').val("");
-      alertify.log("We will keep you posted");
-}
